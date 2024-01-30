756b3046bcdeddedc72414b0b8c616f3ed1f797d
8184993698 <hareeshbandi99@gmail.com>
2024-01-30 12:41:03 +0530

    added changes in code and then adding to local repository

--- src/test/java/temp/git_demo/GitDemoProject.java
+++ src/test/java/temp/git_demo/GitDemoProject.java
@@ -4,6 +4,10 @@ public class GitDemoProject {
 	
 	public void addData(String data) {
 		System.out.println("data = "+data);
+		
+		if(data.equals("java techie")) {
+			System.out.println("Welcome");
+		}
 	}
 
 	public static void main(String[] args) {

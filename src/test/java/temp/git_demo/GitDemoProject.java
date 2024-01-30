bed9b576ed8615ecb7bb33ed726c7643c1556997
8184993698 <hareeshbandi99@gmail.com>
2024-01-30 12:23:03 +0530

    added git-demo project to local repo

---
+++ src/test/java/temp/git_demo/GitDemoProject.java
@@ -0,0 +1,14 @@
+package temp.git_demo;
+
+public class GitDemoProject {
+	
+	public void addData(String data) {
+		System.out.println("data = "+data);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
+	}
+
+}
